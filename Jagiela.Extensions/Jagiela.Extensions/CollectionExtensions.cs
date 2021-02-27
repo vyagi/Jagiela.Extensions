dc3aff7df89feca250336352e897928fc9e9aa0d
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-02-27 16:28:59 +0100

    First tools in the toolbox

---
+++ Jagiela.Extensions/Jagiela.Extensions/CollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Jagiela.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static T Second<T>(this IEnumerable<T> input) =>
+            input.Skip(1).First();
+
+        public static T SecondOrDefault<T>(this IEnumerable<T> input) =>
+            input.Skip(1).FirstOrDefault();
+    }
+}
---
+++ Jagiela.Extensions/Jagiela.Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Jagiela.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string JoinWith(this IEnumerable<string> input, string separator) =>
+            string.Join(separator, input);
+    }
+}
